df3d7775d3d9d95a0b1b6da4fa34232152292c3d
Markus Borg <markus.borg@gmail.com>
2019-10-31 11:47:29 +0100

    Remove obsolete example

--- prescan_repl_sbst/Functions/Fn_MiLTester_SetSimulationTime.m
+++
@@ -1,5 +0,0 @@
-function []=Fn_MiLTester_SetSimulationTime(SimulationRunningTime)
-
-    hAcs = getActiveConfigSet(gcs);
-    set_param(hAcs, 'StopTime', num2str(SimulationRunningTime));
-end
--- prescan_repl_sbst/Functions/GetSimulationTimeStep.m
+++
@@ -1,7 +0,0 @@
-function [SimulationTimeStep]=GetSimulationTimeStep()
-
-    hAcs = getActiveConfigSet(gcs);
-   SimulationTimeStepStr=get_param(hAcs, 'FixedStep');
-  % SimulationTimeStepStr=get_param(hAcs, 'OutputOption');
-    SimulationTimeStep=str2num(SimulationTimeStepStr);
-end
