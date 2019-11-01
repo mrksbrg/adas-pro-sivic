98d0cb5f704ec20ec2e29d8ba97007f59b7f9828
Markus Borg <markus.borg@gmail.com>
2019-11-01 14:46:14 +0100

    Major refactoring. Tested.

--- prescan_repl_minimal/Functions/calc_obj_funcs.m
+++ prescan_repl_minimal/Functions/calc_obj_funcs.m
@@ -2,232 +2,156 @@
 %  Copyright (c) 2019, Markus Borg
 %  All rights reserved.
 %
-%  Redistribution and use in source and binary forms, with or without 
-%  modification, are permitted provided that the following conditions are 
+%  Redistribution and use in source and binary forms, with or without
+%  modification, are permitted provided that the following conditions are
 %  met:
 %
-%     * Redistributions of source code must retain the above copyright 
+%     * Redistributions of source code must retain the above copyright
 %       notice, this list of conditions and the following disclaimer.
-%     * Redistributions in binary form must reproduce the above copyright 
-%       notice, this list of conditions and the following disclaimer in 
+%     * Redistributions in binary form must reproduce the above copyright
+%       notice, this list of conditions and the following disclaimer in
 %       the documentation and/or other materials provided with the distribution
-%      
-%  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-%  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-%  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-%  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
-%  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-%  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-%  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-%  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+%
+%  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+%  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+%  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+%  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+%  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+%  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+%  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+%  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
 %  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-%  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+%  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 %  POSSIBILITY OF SUCH DAMAGE.
 
-function [BestDist2,minTTC,BestDistPAWA]=calcObjFuncs(simOut, ThP)
+function [min_dist, min_ttc, min_dist_awa] = calc_obj_funcs(sim_output, ped_orient)
 
-simulationSteps = length(simOut.xCar.time)
+nbr_sim_steps = length(sim_output.xCar.time);
 
-minTTC=min(simOut.TTCcol.signals.values);
+car_x_array = sim_output.xCar;
+car_y_array = sim_output.yCar;
+car_speed_array = sim_output.vCar;
+ped_x_array = sim_output.xPerson;
+ped_y_array = sim_output.yPerson;
+%ped_speed_array = sim_output.vPerson; % not used for the moment
 
-xCar = simOut.xCar
-yCar = simOut.yCar
-vCar = simOut.vCar
-xPerson = simOut.xPerson
-yPerson = simOut.yPerson
-vPerson = simOut.vPerson
+awa_offset = 10; % the distance between the car and the acute warning area
+length_awa = 50; % this is the length of the acute warning area
 
-d=10;
-L_AWA=50;
+min_dist = 100;
+min_ttc = min(sim_output.TTCcol.signals.values); % simply find in array
+min_dist_awa = 50;
 
-D=1;
-D2=1;
-D3=1;
-BestIndXC=0;
-BestIndVC=0;
-BestIndYC=0;
-% BestIndT=0;
-BestIndXP= 0;
-BestIndY0P=0;
-BestIndVP=0;
-
-BestDist=60;
-BestDist2=100;
-BestDistPAWA=50;
-BESTBESTDistPAWA=50;
-
-
-%Dist=0;
-
-for i=1:simulationSteps,
+for i = 1:nbr_sim_steps
     
-    % Disregard the the heading 0s in the arrays (position not yet ready)
-    if (xPerson.signals.values(i,1) ~= 0 && yPerson.signals.values(i,1) ~= 0 && xCar.signals.values(i,1) ~= 0 && yCar.signals.values(i,1) ~= 0)
+    % extract the positions for time step i
+    car_x_value = car_x_array.signals.values(i, 1);
+    car_y_value = car_y_array.signals.values(i, 1);
+    car_speed_value = car_speed_array.signals.values(i, 1);
+    ped_x_value = ped_x_array.signals.values(i, 1);
+    ped_y_value = ped_y_array.signals.values(i, 1);
+    
+    % disregard the the heading 0s in the arrays (position not yet ready)
+    if (ped_x_value ~= 0 && ped_y_value ~= 0 && car_x_value ~= 0 && car_y_value ~= 0)
         
-        
-        %C=xPerson.time(i,1);
-        %LAWA=vCar.signals.values(i,1)*3.6 *(0.9 + vCar.signals.values(i,1)* 3.6 *0.015)-d;
-        %display(LAWA);
-        %C1=(xPerson.signals.values(i,1) - d - xCar.signals.values(i,1))/vCar.signals.values(i,1);
-        %C2=(yCar.signals.values(i,1) + 0.5 * W_AWA - yPerson.signals.values(i,1))/vPerson.signals.values(i,1);
-        %C3=(xPerson.signals.values(i,1) - d - L - xCar.signals.values(i,1))/vCar.signals.values(i,1);
-        %C4=(yCar.signals.values(i,1) - 0.5 * W_AWA - yPerson.signals.values(i,1))/vPerson.signals.values(i,1);
-        %D=distanceFunction(C, C1, C2, C3, C4);
-        
-        if (vCar.signals.values(i,1)*3.6 < 60*3.6),
-            w = 2.2;
-        elseif (vCar.signals.values(i,1)*3.6  <= 80*3.6),
-            w = 2.2 + 0.0075 * (vCar.signals.values(i,1)*3.6  - 60*3.6) * (vCar.signals.values(i,1)*3.6  - 60*3.6);
+        % Set a constant used to calculate the size of the acute warning
+        % area in front of the car. The constant depends on the speed.
+        if (car_speed_value * 3.6 < 60 * 3.6)
+            dist_weight = 2.2;
+        elseif (car_speed_value * 3.6  <= 80 * 3.6)
+            dist_weight = 2.2 + 0.0075 * (car_speed_value * 3.6  - 60 * 3.6) *...
+                (car_speed_value * 3.6  - 60 * 3.6);
         else
-            w = 3.4;
+            dist_weight = 3.4;
         end
         
-        if ((xPerson.signals.values(i,1) <= xCar.signals.values(i,1) - d) &&  (xPerson.signals.values(i,1) >= (xCar.signals.values(i,1)  - d -L_AWA)) && ( yPerson.signals.values(i,1) < yCar.signals.values(i,1) + 0.5 *w)  && (yPerson.signals.values(i,1) > yCar.signals.values(i,1) - 0.5 *w) )
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%% Pedestrian in position 1 %%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        if ((ped_x_value <= car_x_value - awa_offset) &&...
+                (ped_x_value >= (car_x_value - awa_offset - length_awa)) &&...
+                (ped_y_value < car_y_value + 0.5 * dist_weight) &&...
+                (ped_y_value > car_y_value - 0.5 * dist_weight))
             
-            IndXC=xCar.signals.values(i,1);
-            IndYC=yCar.signals.values(i,1);
-            IndVC=vCar.signals.values(i,1);
-            %IndT=xPerson.time(i,1);
-            IndXP= xPerson.signals.values(i,1);
-            IndY0P=yPerson.signals.values(i,1);
-            IndVP=vPerson.signals.values(i,1);
-            D=0;
-            Dist=sqrt ((IndXP-IndXC).^2 + (IndY0P-IndYC).^2);
+            dist = sqrt((ped_x_value - car_x_value).^2 + (ped_y_value - car_y_value).^2);
             
-            if Dist < BestDist
-                BestDist=Dist;
-                
-                BestIndXC=IndXC;
-                BestIndYC=IndYC;
-                BestIndVC=IndVC;
-                BestIndXP= IndXP;
-                BestIndY0P=IndY0P;
-                BestIndVP=IndVP;
+            % if a minimum distance has been found, save it
+            if dist < min_dist
+                min_dist = dist;
             end
             
-            BestDistPAWA=0; %distance between P and AWA
+            min_dist_awa = 0; % the pedestrian is in the acute warning area
             
-            Dist2=sqrt ((xPerson.signals.values(i,1)-xCar.signals.values(i,1)).^2 + (yPerson.signals.values(i,1)-yCar.signals.values(i,1)).^2);
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            %%% Pedestrian in position 2 %%%
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        elseif ((ped_x_value <= car_x_value - awa_offset) &&...
+                (ped_x_value >= (car_x_value - awa_offset -length_awa)) &&...
+                (ped_y_value >= car_y_value + 0.5 * dist_weight))
             
+            dist = sqrt((ped_x_value - car_x_value).^2 + (ped_y_value - car_y_value).^2);
             
-            if Dist2 < BestDist2
-                BestDist2=Dist2;
-                BestIndXC=xCar.signals.values(i,1);
-                BestIndYC=yCar.signals.values(i,1);
-                BestIndVC=vCar.signals.values(i,1);
-                %BestIndT=xPerson.time(i,1);
-                BestIndXP= xPerson.signals.values(i,1);
-                BestIndY0P=yPerson.signals.values(i,1);
-                BestIndVP=vPerson.signals.values(i,1);
+            % if a minimum distance has been found, save it
+            if dist < min_dist
+                min_dist = dist;
             end
             
+            dist_awa = abs((abs(car_y_value - ped_y_value) + 0.5 *...
+                dist_weight)./sin(ped_orient * 0.0174533)); %distance between P and AWA
             
+            if dist_awa < min_dist_awa
+                min_dist_awa = dist_awa;
+            end
             
-        else if ((xPerson.signals.values(i,1) <= xCar.signals.values(i,1) - d) &&  (xPerson.signals.values(i,1) >= (xCar.signals.values(i,1)  - d -L_AWA)) && (yPerson.signals.values(i,1) >= yCar.signals.values(i,1)+ 0.5 * w))
-                D2=0;
-                Dist2=sqrt ((xPerson.signals.values(i,1)-xCar.signals.values(i,1)).^2 + (yPerson.signals.values(i,1)-yCar.signals.values(i,1)).^2);
-                
-                
-                if Dist2 < BestDist2
-                    BestDist2=Dist2;
-                    BestIndXC=xCar.signals.values(i,1);
-                    BestIndYC=yCar.signals.values(i,1);
-                    BestIndVC=vCar.signals.values(i,1);
-                    %BestIndT=xPerson.time(i,1);
-                    BestIndXP= xPerson.signals.values(i,1);
-                    BestIndY0P=yPerson.signals.values(i,1);
-                    BestIndVP=vPerson.signals.values(i,1);
-                end
-                DistPAWA=abs((abs(yCar.signals.values(i,1)-yPerson.signals.values(i,1))+ 0.5 * w)./sin(ThP*0.0174533)); %distance between P and AWA
-                
-                
-                if DistPAWA < BestDistPAWA
-                    BestDistPAWA=DistPAWA;
-                    
-                    BestIndXC=xCar.signals.values(i,1);
-                    BestIndYC=yCar.signals.values(i,1);
-                    BestIndVC=vCar.signals.values(i,1);
-                    %BestIndT=xPerson.time(i,1);
-                    BestIndXP= xPerson.signals.values(i,1);
-                    BestIndY0P=yPerson.signals.values(i,1);
-                    BestIndVP=vPerson.signals.values(i,1);
-                end
-            else if ((xPerson.signals.values(i,1) <= xCar.signals.values(i,1)) &&  (xPerson.signals.values(i,1) >= (xCar.signals.values(i,1)  -d )))
-                    
-                    
-                    D3=0;
-                    Dist2=sqrt ((xPerson.signals.values(i,1)-xCar.signals.values(i,1)).^2 + (yPerson.signals.values(i,1)-yCar.signals.values(i,1)).^2);
-                    
-                    
-                    if Dist2 < BestDist2
-                        BestDist2=Dist2;
-                        BestIndXC=xCar.signals.values(i,1);
-                        BestIndYC=yCar.signals.values(i,1);
-                        BestIndVC=vCar.signals.values(i,1);
-                        %BestIndT=xPerson.time(i,1);
-                        BestIndXP= xPerson.signals.values(i,1);
-                        BestIndY0P=yPerson.signals.values(i,1);
-                        BestIndVP=vPerson.signals.values(i,1);
-                    end
-                    
-                    if (ThP*0.0174533) < 90
-                        DistPAWA=abs(((xCar.signals.values(i,1))- xPerson.signals.values(i,1) - d)./cos(ThP*0.0174533)); %distance between P and AWA
-                    else
-                        DistPAWA=sqrt ((xCar.signals.values(i,1) -d - xPerson.signals.values(i,1)).^2 + (yCar.signals.values(i,1) + 0.5 * w - yPerson.signals.values(i,1)).^2);
-                    end
-                    
-                    if DistPAWA < BestDistPAWA
-                        BestDistPAWA=DistPAWA;
-                        
-                        BestIndXC=xCar.signals.values(i,1);
-                        BestIndYC=yCar.signals.values(i,1);
-                        BestIndVC=vCar.signals.values(i,1);
-                        %BestIndT=xPerson.time(i,1);
-                        BestIndXP= xPerson.signals.values(i,1);
-                        BestIndY0P=yPerson.signals.values(i,1);
-                        BestIndVP=vPerson.signals.values(i,1);
-                    end
-                else if (xPerson.signals.values(i,1) <= (xCar.signals.values(i,1)  - d -L_AWA)),
-                        
-                        Dist2=sqrt ((xPerson.signals.values(i,1)-xCar.signals.values(i,1)).^2 + (yPerson.signals.values(i,1)-yCar.signals.values(i,1)).^2);
-                        
-                        
-                        if Dist2 < BestDist2
-                            BestDist2=Dist2;
-                            BestIndXC=xCar.signals.values(i,1);
-                            BestIndYC=yCar.signals.values(i,1);
-                            BestIndVC=vCar.signals.values(i,1);
-                            %BestIndT=xPerson.time(i,1);
-                            BestIndXP= xPerson.signals.values(i,1);
-                            BestIndY0P=yPerson.signals.values(i,1);
-                            BestIndVP=vPerson.signals.values(i,1);
-                        end
-                        
-                        if (ThP*0.0174533) > 90
-                            DistPAWA=abs(  (xPerson.signals.values(i,1)- (xCar.signals.values(i,1) - d -L_AWA))./((-1).* cos(ThP*0.0174533))); %distance between P and AWA
-                        else
-                            DistPAWA=sqrt ((xPerson.signals.values(i,1)- (xCar.signals.values(i,1) -d -L_AWA)).^2 + (yPerson.signals.values(i,1)-yCar.signals.values(i,1) + 0.5 *w).^2);
-                        end
-                        
-                        if DistPAWA < BestDistPAWA
-                            BestDistPAWA=DistPAWA;
-                            
-                            BestIndXC=xCar.signals.values(i,1);
-                            BestIndYC=yCar.signals.values(i,1);
-                            BestIndVC=vCar.signals.values(i,1);
-                            %BestIndT=xPerson.time(i,1);
-                            BestIndXP= xPerson.signals.values(i,1);
-                            BestIndY0P=yPerson.signals.values(i,1);
-                            BestIndVP=vPerson.signals.values(i,1);
-                        end
-                    end
-                end
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            %%% Pedestrian in position 3 %%%
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        elseif ((ped_x_value <= car_x_value) && (ped_x_value >= (car_x_value - awa_offset)))
+            
+            dist = sqrt((ped_x_value - car_x_value).^2 + (ped_y_value - car_y_value).^2);
+            
+            % if a minimum distance has been found, save it
+            if dist < min_dist
+                min_dist = dist;
+            end
+            
+            if (ped_orient * 0.0174533) < 90
+                dist_awa = abs(((car_x_value) -...
+                    ped_x_value - awa_offset)./cos(ped_orient * 0.0174533));
+            else
+                dist_awa=sqrt((car_x_value - awa_offset - ped_x_value).^2 +...
+                    (car_y_value + 0.5 * dist_weight - ped_y_value).^2);
+            end
+            
+            if dist_awa < min_dist_awa
+                min_dist_awa = dist_awa;            
+            end
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            %%% Pedestrian in position 4 %%%
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        elseif (ped_x_value <= (car_x_value - awa_offset - length_awa))
+            
+            dist = sqrt((ped_x_value - car_x_value).^2 + (ped_y_value - car_y_value).^2);
+            
+            % if a minimum distance has been found, save it
+            if dist < min_dist
+                min_dist = dist;
+            end
+            
+            if (ped_orient * 0.0174533) > 90
+                dist_awa = abs((ped_x_value -...
+                    (car_x_value - awa_offset - length_awa))./((-1).*...
+                    cos(ped_orient * 0.0174533)));
+            else
+                dist_awa = sqrt((ped_x_value -...
+                    (car_x_value - awa_offset - length_awa)).^2 +...
+                    (ped_y_value - car_y_value + 0.5 * dist_weight).^2);
+            end
+            
+            if dist_awa < min_dist_awa
+                min_dist_awa = dist_awa;              
             end
         end
     end
-    
-    %     if BestDistPAWA < BESTBESTDistPAWA
-    %         BESTBESTDistPAWA=BestDistPAWA;
-    %     end
-    
 end
